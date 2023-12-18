eace9a27a77631ac380629e36b56601ed3dbfb9b
roynaldi3301 <prasbhara0604@gmail.com>
2023-12-18 20:10:05 +0700

    cors mumet jir

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,6 +10,6 @@ class CorsMiddleware
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Methods', '*');
     }
 }
